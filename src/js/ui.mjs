b35df3f6c3d0238dbc57126b3a38fb9e0b837e0d
ofaith@byupathway.edu <ofaith@byupathway.edu>
2025-11-28 21:58:54 +0100

    fix

--- src/js/ui.mjs
+++ src/js/ui.mjs
@@ -28,7 +28,7 @@ export async function renderTemplates() {
 }
 
 export function renderMedia(parentElement, medias) {
-  let type;
+  let type = "";
   let mediasHTML = ``;
   
   const favorites = getLocalStorage() || [];
@@ -39,33 +39,33 @@ export function renderMedia(parentElement, medias) {
         star = "★";
       }
     })
-      if (media.number_of_episodes) {
-        type = "tv"
-          mediasHTML += `
-      <div id="medias">
-        <button id="favorite" data-id="${media.id}" data-type=${type}>${star}</button>
-        <a href="details.html?id=${media.id}&type=${type}">
-          <img src="https://image.tmdb.org/t/p/w300${media.poster_path}" alt="${media.title} Poster" />
-        </a>
-      </div>`
-      } else {
-        type = "movie"
-          mediasHTML += `
-      <div id="medias">
-        <button id="favorite" data-id="${media.id}" data-type=${type}>${star}</button>
-        <a href="details.html?id=${media.id}&type=${type}">
-          <img src="https://image.tmdb.org/t/p/w300${media.poster_path}" alt="${media.title} Poster" />
-        </a>
-      </div>`
-      }   
+
+    if (media.name) {
+      type = "tv"
+    } 
+    if (media.title){
+      type = "movie"  
+    }   
+      mediasHTML += `
+    <div id="medias">
+      <button id="favorite" data-id="${media.id}" data-type=${type}>${star}</button>
+      <a href="details.html?id=${media.id}&type=${type}">
+        <img src="https://image.tmdb.org/t/p/w300${media.poster_path}" alt="${media.title} Poster" />
+      </a>
+    </div>`
   });
   parentElement.innerHTML = mediasHTML;
 }
 
 export function renderDetails(parentElement, media){
+  let title;
+  let date;
+  let duration;
   let type;
-  let detailsHtml = ``;
+  let genres = [];  
 
+  let detailsHtml = ``;
+  console.log(media)
   let star = "☆";
   const favorites = getLocalStorage() || [];
   favorites.forEach(fav =>{
@@ -74,32 +74,38 @@ export function renderDetails(parentElement, media){
     }
   })
 
-  let genres = [];  
+  
   for (let i = 0; i < media.genres.length; i++) {
     genres.push(media.genres[i].name);
   }
-
-  let minutes = media.runtime;
-  let hours = 0;
-  while (minutes > 60){
-    hours += 1;
-    minutes -= 60;
-  }
-
+  
+  
   if (media.number_of_episodes) {
         type = "tv";
-  } else {
+        date = new Date(media.first_air_date);
+        duration = `S${media.number_of_seasons}`;
+        title = media.name;
+  } else if (!media.number_of_episodes){
     type = "movie";
+    title = media.title;
+    let minutes = media.runtime;
+    let hours = 0;
+    while (minutes > 60){
+      hours += 1;
+      minutes -= 60;
+    }
+    duration = `${hours}h ${minutes}m`;
+    date = new Date(media.release_date);
   }
 
-  const date = new Date(media.release_date)
-  document.querySelector("title").textContent = `${media.title} (${date.getFullYear()})`
+  
+  document.querySelector("title").textContent = `${title} (${date.getFullYear()})`;
   detailsHtml += `
     <div id="movie-details">
-      <img src="https://image.tmdb.org/t/p/w300${media.poster_path}" alt="${media.title} Poster" />
+      <img src="https://image.tmdb.org/t/p/w300${media.poster_path}" alt="${title} Poster" />
       <div>
-        <h1>${media.title} (${date.getFullYear()})<button id="favorite" data-id="${media.id}" data-type=${type}>${star}</button></h1>
-        <p>${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()} • ${genres.join(", ")} • ${hours}h ${minutes}m</p>
+        <h1>${title} (${date.getFullYear()})<button id="favorite" data-id="${media.id}" data-type=${type}>${star}</button></h1>
+        <p>${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()} • ${genres.join(", ")} • ${duration}</p>
         <h2>Overview</h2>
         <span>${media.tagline}</span>
         <p>${media.overview}</p>
